e17361accf8c8b4c9338b47e3704d98c864f7bf7
BryanYu <ak78912@hotmail.com>
2018-01-15 22:51:22 +0800

    Ch4Sample

---
+++ AgilePrinciplesPracticeTests/Ch4/WumpusGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AgilePrinciplesPracticeTests.Ch4
+{
+    public class WumpusGame
+    {
+        public double GetPlayerRoom(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void East()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetPlayerRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Connect(int v1, int v2, string v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
