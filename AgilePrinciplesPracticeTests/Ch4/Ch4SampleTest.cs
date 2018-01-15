54b93b5d519f089654394d0f8cf087d09f08a34b
BryanYu <ak78912@hotmail.com>
2018-01-15 22:47:43 +0800

    Init

---
+++ AgilePrinciplesPracticeTests/Ch4/Ch4SampleTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AgilePrinciplesPracticeTests.Ch4
+{
+    [TestFixture]
+    public class Ch4SampleTest
+    {
+        [Test]
+        public void TestMove()
+        {
+            WumpusGame g = new WumpusGame();
+            g.Connect(4, 5, "E");
+            g.GetPlayerRoom(4);
+            g.East();
+            Assert.AreEqual(5, g.GetPlayerRoom());
+        }
+    }
+}
---
+++ AgilePrinciplesPracticeTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AgilePrinciplesPracticeTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AgilePrinciplesPracticeTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59455fcb-49c2-45e4-8b1b-3c94b452e232")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
