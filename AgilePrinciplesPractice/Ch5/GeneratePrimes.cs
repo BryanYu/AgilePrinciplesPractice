6b9d2174c63a0b7cc2dd40c1e012e5046ff14943
BryanYu <ak78912@hotmail.com>
2018-01-16 20:56:51 +0800

    Ch5 - GeneratePrimes Version 4

--- AgilePrinciplesPractice/Ch5/GeneratePrimes.cs
+++ AgilePrinciplesPractice/Ch5/GeneratePrimes.cs
@@ -13,8 +13,8 @@ namespace AgilePrinciplesPractice.Ch5
     public class PrimeGenerator
     {
         private static int s;
-        private static bool[] f;
-        private static int[] primes;
+        private static bool[] isCrossed;
+        private static int[] result;
 
         /// <summary>
         /// 產生一個包含質數的陣列
@@ -30,10 +30,42 @@ namespace AgilePrinciplesPractice.Ch5
             else
             {
                 InitialieArrayOfIntegers(maxValue);
+                CrossOutMultiples();
                 Sieve();
                 LoadPrimes();
-                return primes;
+                return result;
             }
+        }
+
+        private static void CrossOutMultiples()
+        {
+            int maxPrimeFactor = CalcMaxPrimeFactor();
+            for (int i = 2; i < maxPrimeFactor + 1; i++)
+            {
+                if (NotCrossed(i))
+                {
+                    CrossOutMultiplesOf(i);
+                }
+            }
+        }
+
+        private static bool NotCrossed(int i)
+        {
+            return isCrossed[i] == false;
+        }
+
+        private static void CrossOutMultiplesOf(int i)
+        {
+            for (int multiple = 2 * i; multiple < isCrossed.Length; multiple += i)
+            {
+                isCrossed[multiple] = true;
+            }
+        }
+
+        private static int CalcMaxPrimeFactor()
+        {
+            double maxPrimeFactor = Math.Sqrt(isCrossed.Length) + 1;
+            return (int)maxPrimeFactor;
         }
 
         private static void LoadPrimes()
@@ -44,19 +76,19 @@ namespace AgilePrinciplesPractice.Ch5
             int count = 0;
             for (i = 0; i < s; i++)
             {
-                if (f[i])
+                if (isCrossed[i])
                 {
                     count++;
                 }
             }
 
-            primes = new int[count];
+            result = new int[count];
             // 把質數轉移到結果陣列中
             for (i = 0, j = 0; i < s; i++)
             {
-                if (f[i])
+                if (isCrossed[i])
                 {
-                    primes[j++] = i;
+                    result[j++] = i;
                 }
             }
         }
@@ -68,11 +100,11 @@ namespace AgilePrinciplesPractice.Ch5
             int j;
             for (i = 2; i < Math.Sqrt(s) + 1; i++)
             {
-                if (f[i]) // 如果i未被劃掉，就劃掉其倍數
+                if (isCrossed[i]) // 如果i未被劃掉，就劃掉其倍數
                 {
                     for (j = 2 * i; j < s; j += i)
                     {
-                        f[j] = false; // 倍數不是質數
+                        isCrossed[j] = false; // 倍數不是質數
                     }
                 }
             }
@@ -80,12 +112,12 @@ namespace AgilePrinciplesPractice.Ch5
 
         private static void InitialieArrayOfIntegers(int maxValue)
         {
-            f = new bool[maxValue + 1];
-            f[0] = f[1] = false;
+            isCrossed = new bool[maxValue + 1];
+            isCrossed[0] = isCrossed[1] = false;
             //陣列元素初始為true
-            for (int i = 2; i < f.Length; i++)
+            for (int i = 2; i < isCrossed.Length; i++)
             {
-                f[i] = true;
+                isCrossed[i] = true;
             }
         }
     }
