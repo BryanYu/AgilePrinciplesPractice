eac3b355cc9c8cfc5cdc3361b05174607d578b70
BryanYu <ak78912@hotmail.com>
2018-01-16 20:44:37 +0800

    Ch5-GeneratePrimes Version2

--- AgilePrinciplesPractice/Ch5/GeneratePrimes.cs
+++ AgilePrinciplesPractice/Ch5/GeneratePrimes.cs
@@ -9,8 +9,12 @@ namespace AgilePrinciplesPractice.Ch5
     /// <summary>
     /// 產生質數程式
     /// </summary>
-    public class GeneratePrimes
+    public class PrimeGenerator
     {
+        private static int s;
+        private static bool[] f;
+        private static int[] primes;
+
         /// <summary>
         /// 產生一個包含質數的陣列
         /// </summary>
@@ -18,59 +22,74 @@ namespace AgilePrinciplesPractice.Ch5
         /// <returns></returns>
         public static int[] GeneratePrimeNumbers(int maxValue)
         {
-            if (maxValue >= 2)
-            {
-                int s = maxValue + 1;
-                bool[] f = new bool[s];
-                int i;
-                //陣列元素初始為true
-                for (i = 0; i < s; i++)
-                {
-                    f[i] = true;
-                }
-
-                // 去掉已知的非質數
-                f[0] = f[1] = false;
-
-                // sieve (篩選；過濾)
-                int j;
-                for (i = 2; i < Math.Sqrt(s) + 1; i++)
-                {
-                    if (f[i]) // 如果i未被劃掉，就劃掉其倍數
-                    {
-                        for (j = 2 * i; j < s; j += i)
-                        {
-                            f[j] = false; // 倍數不是質數
-                        }
-                    }
-                }
-
-                // 有多少個質數?
-                int count = 0;
-                for (i = 0; i < s; i++)
-                {
-                    if (f[i])
-                    {
-                        count++;
-                    }
-                }
-
-                int[] primes = new int[count];
-                // 把質數轉移到結果陣列中
-                for (i = 0, j = 0; i < s; i++)
-                {
-                    if (f[i])
-                    {
-                        primes[j++] = i;
-                    }
-                }
-
-                return primes; // 回傳primes結果陣列
-            }
-            else //maxValue <2
+            if (maxValue < 2)
             {
                 return new int[0]; //若輸入不合理值，則回傳空陣列
             }
+            else
+            {
+                InitializeSieve(maxValue);
+                Sieve();
+                LoadPrimes();
+                return primes;
+            }
+        }
+
+        private static void LoadPrimes()
+        {
+            // 有多少個質數?
+            int i;
+            int j;
+            int count = 0;
+            for (i = 0; i < s; i++)
+            {
+                if (f[i])
+                {
+                    count++;
+                }
+            }
+
+            primes = new int[count];
+            // 把質數轉移到結果陣列中
+            for (i = 0, j = 0; i < s; i++)
+            {
+                if (f[i])
+                {
+                    primes[j++] = i;
+                }
+            }
+        }
+
+        private static void Sieve()
+        {
+            // sieve (篩選；過濾)
+            int i;
+            int j;
+            for (i = 2; i < Math.Sqrt(s) + 1; i++)
+            {
+                if (f[i]) // 如果i未被劃掉，就劃掉其倍數
+                {
+                    for (j = 2 * i; j < s; j += i)
+                    {
+                        f[j] = false; // 倍數不是質數
+                    }
+                }
+            }
+        }
+
+        private static void InitializeSieve(int maxValue)
+        {
+            s = maxValue + 1;
+            f = new bool[s];
+            int i;
+            //陣列元素初始為true
+            for (i = 0; i < s; i++)
+            {
+                f[i] = true;
+            }
+
+            // 去掉已知的非質數
+            f[0] = f[1] = false;
         }
     }
 }
--- AgilePrinciplesPracticeTests/Ch5/GeneratePrimesTest.cs
+++ AgilePrinciplesPracticeTests/Ch5/GeneratePrimesTest.cs
@@ -14,19 +14,19 @@ namespace AgilePrinciplesPracticeTests.Ch5
         [Test]
         public void TestPrimes()
         {
-            int[] nullArray = GeneratePrimes.GeneratePrimeNumbers(0);
+            int[] nullArray = PrimeGenerator.GeneratePrimeNumbers(0);
             Assert.AreEqual(nullArray.Length, 0);
 
-            int[] minArray = GeneratePrimes.GeneratePrimeNumbers(2);
+            int[] minArray = PrimeGenerator.GeneratePrimeNumbers(2);
             Assert.AreEqual(minArray.Length, 1);
             Assert.AreEqual(minArray[0], 2);
 
-            int[] threeArray = GeneratePrimes.GeneratePrimeNumbers(3);
+            int[] threeArray = PrimeGenerator.GeneratePrimeNumbers(3);
             Assert.AreEqual(threeArray.Length, 2);
             Assert.AreEqual(threeArray[0], 2);
             Assert.AreEqual(threeArray[1], 3);
 
-            int[] centArray = GeneratePrimes.GeneratePrimeNumbers(100);
+            int[] centArray = PrimeGenerator.GeneratePrimeNumbers(100);
             Assert.AreEqual(centArray.Length, 25);
             Assert.AreEqual(centArray[24], 97);
         }
