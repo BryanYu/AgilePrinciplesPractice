d8e4e7b6469765bf33ae9e7176cf06202aec2104
BryanYu <ak78912@hotmail.com>
2018-01-17 20:17:02 +0800

    Ch7Sample

---
+++ AgilePrinciplesPractice/Ch7/Copier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilePrinciplesPractice.Ch7
+{
+    public interface Reader
+    {
+        int Read();
+    }
+
+    public class KeyboardReader : Reader
+    {
+        public int Read()
+        {
+            return Keyboard.Read();
+        }
+    }
+
+    public class Keyboard
+    {
+        public static int Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Copier
+    {
+        public static Reader reader = new KeyboardReader();
+
+        public static void Copy()
+        {
+            int c;
+            while ((c = (reader.Read())) != -1)
+            {
+                Printer.Write(c);
+            }
+        }
+    }
+
+    public class Printer
+    {
+        public static void Write(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
