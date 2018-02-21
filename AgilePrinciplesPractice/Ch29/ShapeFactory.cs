be4b76983a605d922a9fdf958f2099ac885055a4
BryanYu <ak78912@hotmail.com>
2018-02-21 21:54:07 +0800

    Ch29. Factory

---
+++ AgilePrinciplesPractice/Ch29/ShapeFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilePrinciplesPractice.Ch29
+{
+    public interface ShapeFactory
+    {
+        Shape MakeCircle();
+
+        Shape MakeSquare();
+    }
+
+    public interface Shape
+    {
+    }
+
+    public class ShapeFactoryImplementation : ShapeFactory
+    {
+        public Shape MakeCircle()
+        {
+            return new Circle();
+        }
+
+        public Shape MakeSquare()
+        {
+            return new Square();
+        }
+    }
+
+    public class Square : Shape
+    {
+    }
+
+    public class Circle : Shape
+    {
+    }
+}
